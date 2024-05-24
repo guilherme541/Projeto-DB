edb300d247b939f60270348e64a2862c09a12ecf
Guilherme Gomes <50757333+guilherme541@users.noreply.github.com>
2024-05-24 15:54:40 -0300

    Update queries_table.sql

--- queries_table.sql
+++ queries_table.sql
@@ -1,60 +1,60 @@
 SELECT 
-    i.RA,
-    i.DisciplinaID,
-    d.Nome AS NomeDisciplina,
-    i.Semestre,
-    i.Ano,
-    i.NotaFinal
-FROM INSCRICAO_DISCIPLINAS i
-INNER JOIN DISCIPLINAS d ON i.DisciplinaID = d.DisciplinaID
-WHERE i.RA = '960419885'; -- DIGITE UM RA QUE ESTEJA NA TABELA DADOS ALEATORIOS
+    H.RA,
+    H.DisciplinaID,
+    D.Nome AS NomeDisciplina,
+    H.Semestre,
+    H.Ano,
+    H.NotaFinal
+FROM HISTORICO_DISCIPLINAS H
+INNER JOIN DISCIPLINAS d ON H.DisciplinaID = D.DisciplinaID
+WHERE H.RA = '960419885'; -- DIGITE UM RA QUE ESTEJA NA TABELA DADOS ALEATORIOS
 
 
 SELECT 
-    h.ProfessorID,
-    h.DisciplinaID,
-    d.Nome AS NomeDisciplina,
-    h.Semestre,
-    h.Ano
-FROM HISTORICO_DOCENTE h
-INNER JOIN DISCIPLINAS d ON h.DisciplinaID = d.DisciplinaID
-WHERE h.ProfessorID = 'DIGITE UM ID DO PROFESSOR QUE ESTEJA NA TABELA DADOS ALEATORIOS';
+    H.ProfessorID,
+    H.DisciplinaID,
+    D.Nome AS NomeDisciplina,
+    H.Semestre,
+    H.Ano
+FROM HISTORICO_DOCENTE H
+INNER JOIN DISCIPLINAS D ON H.DisciplinaID = D.DisciplinaID
+WHERE H.ProfessorID = 'DIGITE UM ID DO PROFESSOR QUE ESTEJA NA TABELA DADOS ALEATORIOS';
 
 
 SELECT 
-    a.RA,
-    a.Nome,
-    a.Email
-FROM ALUNOS a
+    A.RA,
+    A.Nome,
+    A.Email
+FROM ALUNOS A
 WHERE NOT EXISTS (
         SELECT 
             1
-        FROM  MATRIZ_CURRICULAR mc
-        LEFT JOIN INSCRICAO_DISCIPLINAS id ON mc.DisciplinaID = id.DisciplinaID AND id.RA = a.RA
+        FROM  MATRIZ_CURRICULAR M
+        LEFT JOIN INSCRICAO_DISCIPLINAS id ON M.DisciplinaID = id.DisciplinaID AND id.RA = A.RA
         WHERE id.NotaFinal < 5.0 OR id.RA IS NULL
     )
     AND EXISTS (
-        SELECT 1 FROM INSCRICAO_DISCIPLINAS id
-        WHERE id.RA = a.RA AND id.Ano = ' DIGITE UM ANO QUE ESTEJA NA TABELA DADOS ALEATORIOS'AND id.Semestre = 'DIGITE UM SEMESTRE QUE ESTEJA NA TABELA DADOS ALEATORIOS'
+        SELECT 1 FROM HISTORICO_DISCIPLINAS H
+        WHERE H.RA = A.RA AND H.Ano = ' DIGITE UM ANO QUE ESTEJA NA TABELA DADOS ALEATORIOS'AND H.Semestre = 'DIGITE UM SEMESTRE QUE ESTEJA NA TABELA DADOS ALEATORIOS'
     );
 
 
 SELECT 
-    p.ProfessorID,
-    p.Nome AS NomeProfessor,
-    d.Nome AS NomeDepartamento
-FROM PROFESSORES p
-INNER JOIN DEPARTAMENTOS d ON p.ProfessorID = d.CoordenadorID;
+    P.ProfessorID,
+    P.Nome AS NomeProfessor,
+    D.Nome AS NomeDepartamento
+FROM PROFESSORES P
+INNER JOIN DEPARTAMENTOS D ON P.ProfessorID = D.CoordenadorID;
 
 
 SELECT 
-    gt.GrupoTCCID,
-    gt.Tema,
-    gt.ProfessorOrientadorID,
-    p.Nome AS NomeOrientador,
-    mt.RA,
-    a.Nome AS NomeAluno
-FROM GRUPOS_TCC gt
-INNER JOIN PROFESSORES p ON gt.ProfessorOrientadorID = p.ProfessorID
-INNER JOIN MEMBROS_TCC mt ON gt.GrupoTCCID = mt.GrupoTCCID
-INNER JOIN ALUNOS a ON mt.RA = a.RA;
+    G.GrupoTCCID,
+    G.Tema,
+    G.ProfessorOrientadorID,
+    P.Nome AS NomeOrientador,
+    M.RA,
+    A.Nome AS NomeAluno
+FROM GRUPOS_TCC G
+INNER JOIN PROFESSORES P ON G.ProfessorOrientadorID = P.ProfessorID
+INNER JOIN MEMBROS_TCC M ON G.GrupoTCCID = M.GrupoTCCID
+INNER JOIN ALUNOS A ON M.RA = A.RA;
